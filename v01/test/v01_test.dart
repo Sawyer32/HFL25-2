eac42bc0f1a86d826b69acc7535306f3c7b7bac2
Jesper <jesper.hunesjo@hunet.se>
2025-09-29 14:15:40 +0200

    Skapat enkla testcase för varje operation

--- v01/test/v01_test.dart
+++ v01/test/v01_test.dart
@@ -3,9 +3,15 @@ import 'package:test/test.dart';
 
 void main() {
   test('calculate-addition', () {
-    expect(calculate("3 + 3"), 6);
+    expect(calculate("1 + 1"), 2);
   });
   test('calculate-subtraction', () {
-    expect(calculate("3 - 2"), 1);
+    expect(calculate("3-2"), 1);
+  });
+  test('calculate-multiply', () {
+    expect(calculate('3*3'), 9);
+  });
+  test('calculate-divide', () {
+    expect(calculate('6/2'), 3);
   });
 }
