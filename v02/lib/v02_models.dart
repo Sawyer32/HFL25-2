2c9ae7dcd3d1604562ce0dc6d28ff693a483fbdc
Jesper <jesper.hunesjo@hunet.se>
2025-10-06 19:26:46 +0200

    Mall för klasserna

--- v02/lib/v02_models.dart
+++ v02/lib/v02_models.dart
@@ -7,3 +7,26 @@ enum MenuOptions {
   search,
   exit
 }
+
+class Hero {
+  final String name;
+  int level;
+  final HeroType type;
+  final HeroAttribute attribute;
+
+  Hero(this.name, this.level, this.type, this.attribute);
+}
+
+class HeroType {
+  final String race;
+  final String faction;
+
+  HeroType(this.race, this.faction);
+}
+
+class HeroAttribute {
+  final int Strength;
+  final int Stamina;
+
+  HeroAttribute(this.Strength, this.Stamina);
+}
