ae62cd97ce5ff448a237355880a27388a4be1d1a
Jesper Hunesjö <jesper.hunesjo@hunet.se>
2025-10-06 15:36:29 +0200

    Ta bort import as flaggan för att inte göra utskrivningen av typen så lång vid användande

--- v02/lib/v02_menuOptions.dart
+++ v02/lib/v02_menuOptions.dart
@@ -1,9 +1,9 @@
 
 import 'dart:io';
 import 'package:v02/v02_helpers.dart' as v02_helpers;
-import 'package:v02/v02_models.dart' as v02_models;
+import 'package:v02/v02_models.dart';
 
-v02_models.MenuOptions mainMenu() {
+MenuOptions mainMenu() {
   print("1. Skapa hjälte");
   print("2. Visa alla hjältar");
   print("3. Avsluta");
@@ -12,38 +12,38 @@ v02_models.MenuOptions mainMenu() {
   switch (input) {
     case "1":
       v02_helpers.clearTerminal();
-      return v02_models.MenuOptions.create;
+      return MenuOptions.create;
     case "2": 
       v02_helpers.clearTerminal();
-      return v02_models.MenuOptions.list;
+      return MenuOptions.list;
     case "3":
-      return v02_models.MenuOptions.exit;
+      return MenuOptions.exit;
     default: 
       v02_helpers.clearTerminal();
       print("Ogiltigt val");
-      return v02_models.MenuOptions.main;
+      return MenuOptions.main;
   }
 }
 
-v02_models.MenuOptions listHeroesMenu() {
+MenuOptions listHeroesMenu() {
   while (true) {
     v02_helpers.clearTerminal();
     print("0. Tillbaka");
     final String input = v02_helpers.selectOption();
 
-    if (input == "0") return v02_models.MenuOptions.main;
+    if (input == "0") return MenuOptions.main;
 
     print("Ogiltigt val");
   }
 }
 
-v02_models.MenuOptions createHeroMenu() {
+MenuOptions createHeroMenu() {
     v02_helpers.clearTerminal();
   while (true) {
     stdout.writeln("0. Tillbaka");
 
     final String input = v02_helpers.selectOption();
-    if (input == "0") return v02_models.MenuOptions.main;
+    if (input == "0") return MenuOptions.main;
     print("Ogiltigt val");
   }
 }
