f7574890b7b9421d70a45243876b35525dc79c12
Jesper <jesper.hunesjo@hunet.se>
2025-10-06 20:53:18 +0200

    Skapa modeller för hjälte

--- v02/lib/v02_models.dart
+++ v02/lib/v02_models.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 enum MenuOptions {
   main,
   create,
+  newHero,
   list,
   search,
   exit
@@ -10,11 +11,29 @@ enum MenuOptions {
 
 class Hero {
   final String name;
-  int level;
+  final int level;
   final HeroType type;
-  final HeroAttribute attribute;
+  final HeroAttributes attributes;
 
-  Hero(this.name, this.level, this.type, this.attribute);
+  Hero({required this.name, required this.level, required this.type, required this.attributes});
+
+  factory Hero.fromJson(Map<String, dynamic> json) {
+    return Hero(
+      name: json['name'],
+      level: json['level'],
+      type: HeroType.fromJson(json['type']),
+      attributes: HeroAttributes.fromJson(json['attributes'])
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'level': level,
+      'attributes': attributes.toJson(),
+      'type': type.toJson()
+    };
+  }
 }
 
 class HeroType {
@@ -22,11 +41,33 @@ class HeroType {
   final String faction;
 
   HeroType(this.race, this.faction);
+
+  factory HeroType.fromJson(Map<String, dynamic> json) {
+    return HeroType(json['race'], json['faction']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'race': race,
+      'faction': faction,
+    };
+  }
 }
 
-class HeroAttribute {
-  final int Strength;
-  final int Stamina;
+class HeroAttributes {
+  final int strength;
+  final int stamina;
 
-  HeroAttribute(this.Strength, this.Stamina);
+  HeroAttributes({required this.strength, required this.stamina});
+
+  factory HeroAttributes.fromJson(Map<String, dynamic> json) {
+    return HeroAttributes(strength: json['strength'], stamina: json['stamina']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'strength': strength,
+      'stamina': stamina
+    };
+  }
 }
