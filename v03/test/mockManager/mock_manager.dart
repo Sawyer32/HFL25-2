b389aadf8a3fffd6600de67dc953d90a323e0776
Jesper <jesper.hunesjo@hunet.se>
2025-10-16 19:58:05 +0200

    Tests setup (#43)
    * Ongoing building mock manager
    
    * Added initializing hero test
    
    * Två tester för att initiera en hjälte och för att skapa en hjälte

---
+++ v03/test/mockManager/mock_manager.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:uuid/uuid.dart';
+import 'package:v03/v03_manager.dart';
+import 'package:v03/v03_models.dart';
+
+class MockHeroDataManager implements HeroDataManaging {
+  List<HeroModel> heroes = [];
+  final _uuid = const Uuid();
+
+  @override
+  Future<List<HeroModel>> getHeroList() async {
+    return heroes;
+  }
+
+  @override
+  Future<void> initializeHeroes() async {
+    final HeroModel hero = HeroModel(
+      response: "200",
+      id: "1",
+      name: "Mock",
+      powerstats: HeroPowerStats("10", "20", "100", "90", "300", "123"),
+      biography: HeroBiography(
+        "Mock Hero",
+        ["Ego1", "Ego2"],
+        ["Alias1", "Alias2"],
+        "Mock town",
+        "Mock vol 1",
+        "Mock & Co",
+        "Good",
+      ),
+      appearance: HeroAppearance(
+        "Male",
+        "Human",
+        {'cm': "173", "inches": "5'7"},
+        {'kg': "75", "lbs": "93"},
+        "Black",
+        "Black",
+      ),
+      work: HeroWork("Unemployed", "Basement"),
+      connections: HeroConnection("No affiliation", "None"),
+      image: HeroImage("image url"),
+    );
+    
+    heroes.add(hero);
+  }
+
+  @override
+  Future<void> saveHero(HeroModel hero) async {
+    hero.id ??= _uuid.v4();
+    heroes.add(hero);
+  }
+
+  @override
+  Future<HeroModel?> searchHero(String name) async {
+    try {
+      return heroes.firstWhere((hero) => hero.name.toLowerCase() == name.toLowerCase());
+    } catch (e) {
+      return null;
+    }
+  }
+}
